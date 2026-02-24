416fc106a5fd4ca937817aa2da089b161c2b5cf2
cutehaX0r <97819453+cutehax0r@users.noreply.github.com>
2026-02-24 10:09:55 -0700

    Fix keg_only ordering in proj@0.0.7.rb - must come before depends_on

--- Formula/proj@0.0.7.rb
+++ Formula/proj@0.0.7.rb
@@ -8,11 +8,11 @@ class ProjAT007 < Formula
   sha256 "9c5bb82bb86970450bf28135747b569090f620896eefc367b0d9e9b3df29dd62"
   license "MIT"
 
+  keg_only :versioned_formula
+
   depends_on "cmake" => :build
   depends_on "go" => :build
   depends_on "go-md2man" => :build
-
-  keg_only :versioned_formula
 
   def install
     system "make", "build"
